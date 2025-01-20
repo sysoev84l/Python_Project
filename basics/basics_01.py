539d866cabcc0a59a47a6a6e6c4877ccc5c24de5
sysoev84l <sysoev84l@yandex.ru>
2025-01-20 23:41:57 +0300

    first commit

---
+++ basics/basics_01.py
@@ -0,0 +1,12 @@
+# Определить переменные всех 4 базовых типов и вывести их на экран
+first = 1254 # int
+second = 12.5 # float
+third = '1254' # string
+fourth = True # boolean
+
+print(first)
+print(second)
+print(third)
+print(fourth)
+
+print(first, second, third, fourth)
