c55adc40d6abe4cb64f9063ba2def8f594c99d3f
sysoev84l <sysoev84l@yandex.ru>
2025-01-22 01:45:23 +0300

    collections

---
+++ collections/collections_01.py
@@ -0,0 +1,9 @@
+'''Создать список произвольного содержания.
+ Вывести на экран третий, пятый и шестой элементы'''
+
+my_list = ['boris', 'turecki', 'jonny', 'kirpich', 1, 2, 3, [4, 5, 6]]
+print(my_list[2])
+print(my_list[4])
+print(my_list[5])
+print(my_list[-1])
+
